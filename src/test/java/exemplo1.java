0acaf0375ca82856cf2b7dd7e30d714ebf45938a
jovancosta <52170776+jovancosta@users.noreply.github.com>
2020-06-29 19:43:35 -0300

    Add files via upload...
    Initial Commit

---
+++ src/test/java/exemplo1.java
@@ -0,0 +1,26 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class exemplo1 {
+
+    @Test
+    public void exemplogt() {
+        RestAssured.baseURI = "https://pokeapi.co/api/v2/";
+        RequestSpecification request = RestAssured.given();
+        Response response = request.get("pokemon/1/");
+
+        System.out.println(response.statusCode());
+
+        Assert.assertEquals(200, response.statusCode());
+
+        System.out.println(response.jsonPath().get("forms[0].name").toString());
+        Assert.assertEquals("bauaalbasaur", response.jsonPath().get("forms[0].name").toString());
+    }
+}
